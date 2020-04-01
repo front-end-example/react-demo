2d55607778da072eaf8ce074026c87465afff823
Dong <donly@magicalboy.com>
2020-04-02 00:07:04 +0800

    add clock

--- src/App.js
+++ src/App.js
@@ -2,14 +2,47 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function FormattedDate(props) {
+  return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => {
+      this.tick()
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  render() {
+    return(
+      <div>
+        <FormattedDate date={this.state.date} />
+      </div>
+    );
+  }
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Clock />
         <a
           className="App-link"
           href="https://reactjs.org"
